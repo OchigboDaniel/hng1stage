363bb07e42e7bd181e06592d6a807f6566227af4
unknown <ochigbodaniel240@gmail.com>
2024-06-30 22:05:32 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+
+const app = express();
+
+//app.set("trust proxy", true)
+
+// port
+PORT = 3000
+
+//route
+app.get("/", (req, res)=>{
+    const visitor = req.query.visitors_name 
+
+    const ipAddress = 
+    req.ip ||
+    req.headers['cf-connection-ip'] ||
+    req.headers['x-real-ip'] ||
+    req.headers['x-forwarded-for'] 
+    res.status(200).json({
+        ip: `${ipAddress}`,
+        message: `${visitor}`,
+
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App is runing on ${PORT}`)
+})
