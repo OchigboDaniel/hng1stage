40170735b7cab914f65e5720d75f498d4e0d3055
unknown <ochigbodaniel240@gmail.com>
2024-07-02 17:12:54 -0700

    added cors

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ dotenv.config({path: "./.env"});
 
 const request = require("request");
 const axios = require("axios");
+const cors = require("cors")
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res)=>{
     res.status(200).json({message: "Hello welcome visitor"})
